a49a11ec068135b4fa71a4a464ba68487082f349
jrmesquita7 <cleobetojunior1999@gmail.com>
2023-10-25 12:04:18 -0300

    integrado firebase e inicializado

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:votacao_app/firebase_options.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
