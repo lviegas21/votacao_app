8d0b3cd8a55a0d2e271b38e904000e5075360239
jrmesquita7 <cleobetojunior1999@gmail.com>
2023-10-28 22:04:46 -0300

    criado tela login

--- lib/presenter/login/login_page.dart
+++ lib/presenter/login/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:votacao_app/presenter/login/widget/login_button.dart';
+//import 'package:flutter/widgets.dart';
+//import 'package:votacao_app/presenter/login/widget/login_button.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,9 +13,75 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: LoginButtom(),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 24, 30, 56),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 50),
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 32, 37, 61),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      bottomRight: Radius.circular(60)),
+                  boxShadow: [
+                    BoxShadow(blurRadius: 8.0),
+                  ]),
+              width: MediaQuery.of(context).size.width,
+              height: 500,
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 70, bottom: 50),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  SizedBox(
+                    width: 300,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.person),
+                          hintText: 'Email',
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    width: 300,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.lock),
+                          hintText: 'Senha',
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  const LoginButtom()
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
