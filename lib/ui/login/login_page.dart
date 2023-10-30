16fa2e27e355bd5326ec06477f7b9f84ee22704f
jrmesquita7 <cleobetojunior1999@gmail.com>
2023-10-30 20:18:46 -0300

    adicionado SingleChildScrollView

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:votacao_app/shared/componets/theme/app_color.dart';
 import 'package:votacao_app/ui/login/widget/cadastrar_button.dart';
 import 'package:votacao_app/ui/login/widget/login_button.dart';
 import 'package:votacao_app/ui/login/widget/text_fild.dart';
-import 'package:votacao_app/shared/componets/theme/app_color.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -16,58 +16,66 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.backgroundLogin,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 50),
-            child: Container(
-              decoration: const BoxDecoration(
-                  color: AppColor.backgroudContainerLogin,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(60),
-                      bottomRight: Radius.circular(60)),
-                  boxShadow: [
-                    BoxShadow(blurRadius: 8.0),
-                  ]),
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(top: 70, bottom: 50),
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.white),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.only(top: 150),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 50),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        color: AppColor.backgroudContainerLogin,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(60),
+                            bottomRight: Radius.circular(60)),
+                        boxShadow: [
+                          BoxShadow(blurRadius: 8.0),
+                        ]),
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(top: 70, bottom: 50),
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        InputField(
+                            prefixIcon: const Icon(Icons.person),
+                            hintText: "Email"),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        InputField(
+                          prefixIcon: const Icon(Icons.lock),
+                          hintText: "Senha",
+                          obscureSenha: true,
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        const LoginButtom(),
+                        const SizedBox(height: 20),
+                        const CadastrarButton()
+                      ],
                     ),
                   ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  InputField(
-                      prefixIcon: const Icon(Icons.person), hintText: "Email"),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  InputField(
-                    prefixIcon: const Icon(Icons.lock),
-                    hintText: "Senha",
-                    obscureSenha: true,
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  const LoginButtom(),
-                  const SizedBox(height: 20),
-                  const CadastrarButton()
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
